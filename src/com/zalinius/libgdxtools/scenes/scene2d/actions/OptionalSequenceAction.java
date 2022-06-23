dedbd1fe5d146e01099ea271fd0aba88141d96c8
Zachary Lapointe <lapointez@hotmail.ca>
2022-06-23 17:27:52 -0400

    OptionalAction and OptionalSequenceAction with tests

---
+++ src/com/zalinius/libgdxtools/scenes/scene2d/actions/OptionalSequenceAction.java
@@ -0,0 +1,90 @@
+package com.zalinius.libgdxtools.scenes.scene2d.actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Pool;
+
+/**
+ * A sequence of OptionalAction, which will instantly skip an action if it isn't supposed to be executed.
+ * Effectively, in a single act call, OptionalActions are called until one is enable. 
+ */
+public class OptionalSequenceAction extends Action{
+
+	private List<OptionalAction> optionalActions;
+	private int index;
+
+	public OptionalSequenceAction (OptionalAction... actions) {
+		this.optionalActions = new ArrayList<>();
+		for (int i = 0; i < actions.length; i++) {
+			addAction(actions[i]);
+		}
+	}
+
+	@Override
+	public boolean act (float delta) {
+		if (index >= optionalActions.size()) {
+			return true;
+		}
+		Pool pool = getPool();
+		setPool(null); // Ensure this action can't be returned to the pool while executings.
+		try {
+			while (index < optionalActions.size()) {
+				OptionalAction action = optionalActions.get(index);
+				if(action.shouldAct()) {
+					boolean actionDone = action.act(delta);
+					if(!actionDone) {
+						return false;
+					}
+					else {
+						index++;
+						return index >= optionalActions.size();
+					}
+				}
+				else {
+					index++;
+					if (index >= optionalActions.size()) {
+						return true;
+					}
+				}
+			}
+
+			return true;
+		} finally {
+			setPool(pool);
+		}
+	}
+
+	@Override
+	public void restart () {
+		super.restart();
+		index = 0;
+	}
+	
+	public void addAction (OptionalAction optionalAction) {
+		optionalActions.add(optionalAction);
+		if (actor != null) {
+			optionalAction.setActor(actor);
+		}
+	}
+	
+	/**
+	 * Adds an action that is non optional
+	 * @param action
+	 */
+	public void addMandatoryAction(Action action) {
+		OptionalAction mandatoryAction = new OptionalAction(() -> true);
+		mandatoryAction.setAction(action);
+		addAction(mandatoryAction);
+	}
+
+	@Override
+	public void setActor (Actor actor) {
+		optionalActions.forEach(action -> action.setActor(actor));
+		super.setActor(actor);
+	}
+
+
+}
---
+++ test/com/zalinius/libgdxtools/scenes/scene2d/actions/OptionalActionTest.java
@@ -0,0 +1,42 @@
+package com.zalinius.libgdxtools.scenes.scene2d.actions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.scenes.scene2d.actions.DelegateAction;
+import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
+
+public class OptionalActionTest {
+
+	@Test
+	void act_whenOptionalActionHasSupplierReturningFalse_hasNoEffect() throws Exception {
+		AtomicBoolean spy = new AtomicBoolean(false);
+		RunnableAction runnableAction = new RunnableAction();
+		runnableAction.setRunnable(() -> spy.set(true));
+		DelegateAction optionalAction = new OptionalAction(() -> false);
+		optionalAction.setAction(runnableAction);
+		
+		boolean actionDone = optionalAction.act(0);
+		
+		assertFalse(spy.get());
+		assertTrue(actionDone);
+	}
+	
+	@Test
+	void act_whenOptionalActionHasSupplierReturningTrue_activatesAction() throws Exception {
+		AtomicBoolean spy = new AtomicBoolean(false);
+		RunnableAction runnableAction = new RunnableAction();
+		runnableAction.setRunnable(() -> spy.set(true));
+		DelegateAction optionalAction = new OptionalAction(() -> true);
+		optionalAction.setAction(runnableAction);
+		
+		boolean actionDone = optionalAction.act(0);
+		
+		assertTrue(spy.get());
+		assertTrue(actionDone);
+	}
+		
+}

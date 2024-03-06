67153a809cb24f17170229d4d62c24c987ba8a6d
Darzington <nclparmentier@gmail.com>
2024-03-06 11:39:21 -0500

    Working on flashing slider styles

--- src/main/java/com/darzalgames/libgdxtools/ui/input/keyboard/button/KeyboardSlider.java
+++ src/main/java/com/darzalgames/libgdxtools/ui/input/keyboard/button/KeyboardSlider.java
@@ -3,10 +3,12 @@ package com.darzalgames.libgdxtools.ui.input.keyboard.button;
 import java.util.function.Consumer;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider.SliderStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.darzalgames.darzalcommon.functional.Runnables;
 import com.darzalgames.libgdxtools.ui.input.Input;
 import com.darzalgames.libgdxtools.ui.input.strategy.InputStrategyManager;
 
@@ -16,7 +18,11 @@ public class KeyboardSlider extends KeyboardButton {
 	private float previousValue;
 
 	public KeyboardSlider(TextButton textButton, SliderStyle sliderStyle, Consumer<Float> consumer, InputStrategyManager inputStrategyManager) {
-		super(textButton, inputStrategyManager);
+		this(textButton, null, sliderStyle, consumer, inputStrategyManager);
+	}
+
+	public KeyboardSlider(TextButton textButton, Image image, SliderStyle sliderStyle, Consumer<Float> consumer, InputStrategyManager inputStrategyManager) {
+		super(textButton, image, Runnables.nullRunnable(), inputStrategyManager);
 		slider = new Slider(0, 1, 0.1f, false, sliderStyle);
 		slider.addListener(new ChangeListener() {
 			@Override
@@ -40,7 +46,10 @@ public class KeyboardSlider extends KeyboardButton {
 		});
 	}
 
-	
+	public void setSliderStyle(SliderStyle sliderStyle) {
+		slider.setStyle(sliderStyle);
+	}
+
 	@Override
 	public void consumeKeyInput(Input input) {
 		if (input == Input.LEFT || input == Input.RIGHT) {
@@ -48,7 +57,7 @@ public class KeyboardSlider extends KeyboardButton {
 			slider.setValue(value);
 		}
 	}
-	
+
 	/**
 	 * Set the position of the slider, optionally triggering the sound effect for doing so
 	 * @param newPosition A value within the bar's min/max range, it's clamped regardless
--- src/main/java/com/darzalgames/libgdxtools/ui/input/keyboard/button/UserInterfaceFactory.java
+++ src/main/java/com/darzalgames/libgdxtools/ui/input/keyboard/button/UserInterfaceFactory.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider.SliderStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
@@ -136,7 +137,7 @@ public class UserInterfaceFactory {
 	}
 
 	/**
-	 * Make a LIBGDX textbutton, only to be used inside nested buttons (e.g. checkboxes & sliders)
+	 * Make a LIBGDX textbutton, only to be used inside nested buttons (e.g. checkboxes and sliders)
 	 * @param text
 	 * @return
 	 */
@@ -229,8 +230,8 @@ public class UserInterfaceFactory {
 					button.setStyle(mainButtonStyle);
 					if (shouldButtonFlash(button)) {
 						button.clearActions();
-						Action flashAfterDelay = getChangeStyleAfterDelayAction(button, flashedButtonStyle, flashTime);
-						Action showAfterDelay = getChangeStyleAfterDelayAction(button, mainButtonStyle, flashTime);
+						Action flashAfterDelay = getChangeButtonStyleAfterDelayAction(button, flashedButtonStyle, flashTime);
+						Action showAfterDelay = getChangeButtonStyleAfterDelayAction(button, mainButtonStyle, flashTime);
 						button.addAction(new InstantForeverAction(new InstantSequenceAction(
 								flashAfterDelay,
 								showAfterDelay
@@ -250,6 +251,38 @@ public class UserInterfaceFactory {
 			}
 		});
 	}
+	protected static void makeSliderFlashing(KeyboardSlider keyboardSlider, SliderStyle mainStyle, SliderStyle flashedStyle) {
+		Button button = keyboardSlider.getView(); 
+		button.addListener(new ClickListener() {
+			@Override
+			public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+				super.enter(event, x, y, pointer, fromActor);
+				float flashTime = 1f / 2.5f; // TODO Changes to be the BPS once the audio stuff is in this library
+				if (KeyboardStage.isHoverEvent(pointer) && button.isTouchable()) {
+					keyboardSlider.setSliderStyle(mainStyle);
+					if (shouldButtonFlash(button)) {
+						button.clearActions();
+						Action flashAfterDelay = getChangeSliderStyleAfterDelayAction(keyboardSlider, flashedStyle, flashTime);
+						Action showAfterDelay = getChangeSliderStyleAfterDelayAction(keyboardSlider, mainStyle, flashTime);
+						button.addAction(new InstantForeverAction(new InstantSequenceAction(
+								flashAfterDelay,
+								showAfterDelay
+								)));
+
+					}
+				}
+			}
+
+			@Override
+			public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+				super.exit(event, x, y, pointer, toActor);
+				if (KeyboardStage.isHoverEvent(pointer)) {
+					button.clearActions();
+					keyboardSlider.setSliderStyle(mainStyle);
+				}
+			}
+		});
+	}
 
 	private static boolean shouldButtonFlash(Button button) {
 		return KeyboardStage.isInTouchableBranch(button)
@@ -257,10 +290,22 @@ public class UserInterfaceFactory {
 				&& inputStrategyManager.shouldFlashButtons();
 	}
 
-	private static Action getChangeStyleAfterDelayAction(Button button, ButtonStyle buttonStyle, float delay) {
+	private static Action getChangeButtonStyleAfterDelayAction(Button button, ButtonStyle buttonStyle, float delay) {
 		RunnableAction change = Actions.run(() -> {
 			if (shouldButtonFlash(button)) {
 				button.setStyle(buttonStyle);
+			}
+		});
+		DelayAction changeAfterDelay = Actions.delay(delay);
+		changeAfterDelay.setAction(change);
+
+		return changeAfterDelay;
+	}
+	
+	private static Action getChangeSliderStyleAfterDelayAction(KeyboardSlider keyboardSlider, SliderStyle sliderStyle, float delay) {
+		RunnableAction change = Actions.run(() -> {
+			if (shouldButtonFlash(keyboardSlider.getView())) {
+				keyboardSlider.setSliderStyle(sliderStyle);
 			}
 		});
 		DelayAction changeAfterDelay = Actions.delay(delay);

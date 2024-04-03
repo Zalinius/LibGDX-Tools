0a6e76d24a5267c07ccc4640c78aa79823b326c1
Darzington <nclparmentier@gmail.com>
2024-04-03 12:24:13 -0400

    bits

--- src/main/java/com/darzalgames/libgdxtools/ui/input/keyboard/button/UserInterfaceFactory.java
+++ src/main/java/com/darzalgames/libgdxtools/ui/input/keyboard/button/UserInterfaceFactory.java
@@ -259,9 +259,9 @@ public class UserInterfaceFactory {
 				super.enter(event, x, y, pointer, fromActor);
 				float flashTime = 1f / 2.5f; // TODO Changes to be the BPS once the audio stuff is in this library
 				if (KeyboardStage.isHoverEvent(pointer) && button.isTouchable()) {
+					button.clearActions();
 					keyboardSlider.setSliderStyle(flashedStyle);
 					if (shouldButtonFlash(button)) {
-						button.clearActions();
 						Action flashAfterDelay = getChangeSliderStyleAfterDelayAction(keyboardSlider, flashedStyle, flashTime);
 						Action normalAfterDelay = getChangeSliderStyleAfterDelayAction(keyboardSlider, mainStyle, flashTime);
 						button.addAction(new InstantForeverAction(new InstantSequenceAction(

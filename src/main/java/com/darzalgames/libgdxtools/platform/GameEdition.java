2434714db76e9976f1b64b2d09106c5d0cbbda47
Zalinius <zalinius@darzalgames.com>
2024-11-06 13:36:35 -0500

    .

--- src/main/java/com/darzalgames/libgdxtools/platform/GameEdition.java
+++ src/main/java/com/darzalgames/libgdxtools/platform/GameEdition.java
@@ -1,5 +1,8 @@
 package com.darzalgames.libgdxtools.platform;
 
+/**
+ * Enum for Demo and Full versions of a game
+ */
 public enum GameEdition {
 	DEMO("-demo"),
 	FULL("");
@@ -11,6 +14,9 @@ public enum GameEdition {
 		this.versionSuffix = versionSuffix;
 	}
 
+	/**
+	 * @return A string suffix to describe a version of a game, based on GameEdition
+	 */
 	public String getVersionSuffix() {
 		return versionSuffix;
 	}

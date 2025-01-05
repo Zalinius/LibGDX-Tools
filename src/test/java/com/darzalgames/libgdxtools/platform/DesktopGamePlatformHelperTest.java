ff6d212abe785042c58dfabdfc9bf8fa03e1aefb
Darzington <32342957+Darzington@users.noreply.github.com>
2025-01-05 10:49:41 -0500

    Mac Build Support (#46)
    * Set up MacGamePlatform
    
    * start in fullscreen (not borderless) since mac & linux don't support borderless
    
    * increment version
    
    * use correct save path
    
    * use latest version of common
    
    ---------
    
    Co-authored-by: Darzington <>

--- src/test/java/com/darzalgames/libgdxtools/platform/DesktopGamePlatformHelperTest.java
+++ src/test/java/com/darzalgames/libgdxtools/platform/DesktopGamePlatformHelperTest.java
@@ -9,28 +9,35 @@ class DesktopGamePlatformHelperTest {
 
 	@Test
 	void getGamePlatform_withWindowsInArguments_returnsWindows() throws Exception {
-		GamePlatform result = DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"windows"}, WindowsGamePlatform::new, LinuxGamePlatform::new);
+		GamePlatform result = DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"windows"}, WindowsGamePlatform::new, LinuxGamePlatform::new, MacGamePlatform::new);
 		
 		assertEquals(WindowsGamePlatform.class, result.getClass());
 	}
 	
 	@Test
 	void getGamePlatform_withWindowsAmongOtherArguments_returnsWindows() throws Exception {
-		GamePlatform result = DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"skip-menu", "windows", "garbage"}, WindowsGamePlatform::new, LinuxGamePlatform::new);
+		GamePlatform result = DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"skip-menu", "windows", "garbage"}, WindowsGamePlatform::new, LinuxGamePlatform::new, MacGamePlatform::new);
 		
 		assertEquals(WindowsGamePlatform.class, result.getClass());
 	}
 
 	@Test
 	void getGamePlatform_withLinux_returnsLinux() throws Exception {
-		GamePlatform result = DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"linux"}, WindowsGamePlatform::new, LinuxGamePlatform::new);
+		GamePlatform result = DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"linux"}, WindowsGamePlatform::new, LinuxGamePlatform::new, MacGamePlatform::new);
 		
 		assertEquals(LinuxGamePlatform.class, result.getClass());
 	}
 
 	@Test
+	void getGamePlatform_withMac_returnsMac() throws Exception {
+		GamePlatform result = DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"mac"}, WindowsGamePlatform::new, LinuxGamePlatform::new, MacGamePlatform::new);
+		
+		assertEquals(MacGamePlatform.class, result.getClass());
+	}
+
+	@Test
 	void getGamePlatform_withNoValidPlatform_throwsIllegalArgumentException() throws Exception {
-		assertThrows(IllegalArgumentException.class, () -> DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"dos"}, WindowsGamePlatform::new, LinuxGamePlatform::new));
+		assertThrows(IllegalArgumentException.class, () -> DesktopGamePlatformHelper.getTypeFromArgs(new String[]{"dos"}, WindowsGamePlatform::new, LinuxGamePlatform::new, MacGamePlatform::new));
 	}
 
 }

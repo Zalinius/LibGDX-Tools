cbef1cc0ffb7e0f93b929c54124027c16cf9a01b
Darzington <nclparmentier@gmail.com>
2025-01-28 11:48:16 -0500

    Better faked input for the integration tests, plus a couple more

--- src/test/java/com/darzalgames/libgdxtools/maingame/InitializationIT.java
+++ src/test/java/com/darzalgames/libgdxtools/maingame/InitializationIT.java
@@ -6,9 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.darzalgames.libgdxtools.platform.GamePlatform;
-import com.darzalgames.libgdxtools.ui.input.Input;
 
 class InitializationIT {
 
@@ -23,15 +23,59 @@ class InitializationIT {
 	}
 
 	@Test
-	void launchGame_pressingEscape_pausesTheGame() {
+	void pressingEscape_pausesTheGame() {
 		TestGame.testLauncher(new String[] {GamePlatform.WINDOWS}, 
 				app -> {
-					app.inputReceiver.processKeyInput(Input.getInputFromKey(Keys.ESCAPE));
-					
+					pressKey(Keys.ESCAPE);
+
 					assertTrue(app.pause.isPaused());
-					
+
 					Gdx.app.exit();
 				});
+	}
+
+	@Test
+	void clickingTheOptionsButton_pausesTheGame() {
+		TestGame.testLauncher(new String[] {GamePlatform.WINDOWS}, 
+				app -> {
+					clickMouse(10, 10);
+
+					assertTrue(app.pause.isPaused());
+
+					Gdx.app.exit();
+				});
+	}
+
+	@Test
+	void clickingTheQuitButton_quitsTheGame() {
+		TestGame.testLauncher(new String[] {GamePlatform.WINDOWS}, 
+				app -> {
+					clickMouse(640, 550);
+				});
+		// We made it out of the game
+		assertTrue(true);
+	}
+
+	@Test
+	void navigatingByKeyboard_andPressingTheQuitButton_wrapsAroundTheMenuAndQuitsTheGame() {
+		TestGame.testLauncher(new String[] {GamePlatform.WINDOWS}, 
+				app -> {
+					pressKey(Keys.DOWN); // Switch to keyboard mode
+					pressKey(Keys.UP); // Wrap around to the bottom of the menu
+					pressKey(Keys.ENTER);
+				});
+		// We made it out of the game
+		assertTrue(true);
+	}
+
+	private void pressKey(int key) {
+		Gdx.input.getInputProcessor().keyDown(key);
+		Gdx.input.getInputProcessor().keyUp(key);
+	}
+
+	private void clickMouse(int x, int y) {
+		Gdx.input.getInputProcessor().touchDown(x, y, 0, Buttons.LEFT);
+		Gdx.input.getInputProcessor().touchUp(x, y, 0, Buttons.LEFT);
 	}
 
 }

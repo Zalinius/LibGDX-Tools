0f0660f4ad79ed81f6154f0eb52c2ba12b6a719c
Zalinius <31998081+Zalinius@users.noreply.github.com>
2025-03-14 14:25:27 -0400

    LibgdxTools Online crash reporting (#56)
    * online reporting prototype
    
    * crash popup
    
    * formatting and scroll bar
    
    * font and percentage sizing
    
    * tweaks and stack trace as array
    
    * localization
    
    * import
    
    * display absolute path locally
    
    * space before punctuation in french
    
    * Update ReportStatus.java
    
    * corrections and focus improvement
    
    * don't report on mac, and request focus
    
    ---------
    
    Co-authored-by: Zalinius <zalinius@darzalgames.com>
    Co-authored-by: Darzington <32342957+Darzington@users.noreply.github.com>

--- src/main/java/com/darzalgames/libgdxtools/errorhandling/CrashHandler.java
+++ src/main/java/com/darzalgames/libgdxtools/errorhandling/CrashHandler.java
@@ -1,12 +1,12 @@
 package com.darzalgames.libgdxtools.errorhandling;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.function.Supplier;
@@ -16,8 +16,8 @@ public abstract class CrashHandler {
 	
 	public void handleException(Exception exception, String[] programArguments) throws Exception {
 		CrashReport crashReport = buildCrashReport(exception, programArguments);
-		String status = reportCrash(crashReport);
-		logCrashReportStatus(status);
+		List<ReportStatus> statuses = reportCrash(crashReport);
+		logCrashReportStatus(statuses);
 
 		throw exception;
 	}
@@ -26,12 +26,12 @@ public abstract class CrashHandler {
 	 * @param crashReport
 	 * @return The result of the crash reporting
 	 */
-	public abstract String reportCrash(CrashReport crashReport);
+	public abstract List<ReportStatus> reportCrash(CrashReport crashReport);
 	
 	/**
-	 * @param status What the result of reporting the crash was
+	 * @param statuses What the results of reporting the crash was
 	 */
-	public abstract void logCrashReportStatus(String status);
+	public abstract void logCrashReportStatus(List<ReportStatus> statuses);
 	
 	
 	public static CrashReport buildCrashReport(Exception exception, String[] args) {
@@ -41,7 +41,7 @@ public abstract class CrashHandler {
 		String platform = tryGetString(() -> args[0]);
 		Instant utcTime = Instant.now();
 		UUID id = UUID.randomUUID();
-		String stackTrace = getStackTraceString(exception);
+		String[] stackTrace = getMessageAndStackTraceArray(exception);
 		return new CrashReport(gameName, gameVersion, platform, utcTime, id, stackTrace);
 	}
 	
@@ -68,7 +68,7 @@ public abstract class CrashHandler {
 		return string;
 	}
 	
-	public static String getStackTraceString(Exception exception) {
+	public static String[] getMessageAndStackTraceArray(Exception exception) {
 	    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 	    final Charset utf8 = StandardCharsets.UTF_8;
 	    String data = null;
@@ -76,7 +76,12 @@ public abstract class CrashHandler {
 	    	exception.printStackTrace(ps);
 		    data = baos.toString(utf8);
 	    }
-	    return data;
+	    
+	    String[] stackTraceArray = data.split("\n");
+	    for (int i = 0; i < stackTraceArray.length; i++) {
+			stackTraceArray[i] = stackTraceArray[i].trim();
+		}
+	    return stackTraceArray;
 	}
 	
 }
---
+++ src/main/java/com/darzalgames/libgdxtools/errorhandling/CrashLocalization.java
@@ -0,0 +1,131 @@
+package com.darzalgames.libgdxtools.errorhandling;
+
+import java.util.Set;
+
+public class CrashLocalization {
+	
+	public static final CrashLocalization ENGLISH_CRASH_LOCALIZATION = new CrashLocalization(
+			"{game} - crash reporting",
+			"Unfortunately {game} has crashed :(",
+			"The following report was saved to: {file}",
+			"Crash Report:",
+
+			"Send to DarZal Games",
+			"Sending . . .", 
+			"SENT!  thanks c:",
+			"FAILED({httpCode})  :l",
+			
+			"Copy crash report", 
+			"Copied!",
+			
+			"Exit");
+	
+	public static final CrashLocalization FRENCH_CRASH_LOCALIZATION = new CrashLocalization(
+			"{game} - rapport d'erreur",
+			"Malheureusement, {game} a rencontré une erreur :(",
+			"Le rapport suivant a été sauvegardé à : {file}",
+			"Rapport d'erreur :",
+			
+			"Envoyer à Jeux DarZal",
+			"En cours d'envoi . . .", 
+			"ENVOYÉ !  merci c:",
+			"ÉCHEC({httpCode})  :l",
+			
+			"Copier le rapport", 
+			"Copié !",
+			
+			"Quitter");
+
+	
+	public static CrashLocalization getLocalizationFromCode(String languageCode) {
+		Set<String> frenchLanguageCodes = Set.of("fr", "fra", "fre");
+		
+		if(frenchLanguageCodes.contains(languageCode)) {
+			return FRENCH_CRASH_LOCALIZATION;
+		}
+		else {
+			return ENGLISH_CRASH_LOCALIZATION;
+		}
+	}
+
+	
+	
+	private final String titleSuffixString;
+	private final String reportSituationLabelString;
+	private final String reportFileLabelString;
+	private final String reportHeaderLabelString;
+	
+	private final String sendButtonString;
+	private final String sendingButtonString;
+	private final String sentSuccessButtonString;
+	private final String sentFailedButtonString;
+
+	private final String copyButtonString;
+	private final String copiedButtonString;
+
+	private final String exitButtonString;
+
+	public CrashLocalization(
+			String titleSuffixString, String reportSituationLabelString, String reportFileLabelString, String reportHeaderLabelString,
+			String sendButtonString, String sendingButtonString, String sentSuccessButtonString, String sentFailedButtonString,
+			String copyButtonString, String copiedButtonString, String exitButtonString) {
+		super();
+		this.titleSuffixString = titleSuffixString;
+		this.reportSituationLabelString = reportSituationLabelString;
+		this.reportFileLabelString = reportFileLabelString;
+		this.reportHeaderLabelString = reportHeaderLabelString;
+		this.sendButtonString = sendButtonString;
+		this.sendingButtonString = sendingButtonString;
+		this.sentSuccessButtonString = sentSuccessButtonString;
+		this.sentFailedButtonString = sentFailedButtonString;
+		this.copyButtonString = copyButtonString;
+		this.copiedButtonString = copiedButtonString;
+		this.exitButtonString = exitButtonString;
+	}
+
+	public String getTitleSuffixString(String game) {
+		return titleSuffixString.replace("{game}", game);
+	}
+
+	public String getReportSituationLabelString(String game) {
+		return reportSituationLabelString.replace("{game}", game);
+	}
+
+	public String getReportFileLabelString(String file) {
+		return reportFileLabelString.replace("{file}", file);
+	}
+
+	public String getReportHeaderLabelString() {
+		return reportHeaderLabelString;
+	}
+
+	public String getSendButtonString() {
+		return sendButtonString;
+	}
+
+	public String getSendingButtonString() {
+		return sendingButtonString;
+	}
+
+	public String getSentSuccessButtonString() {
+		return sentSuccessButtonString;
+	}
+
+	public String getSentFailedButtonString(String statusCode) {
+		return sentFailedButtonString.replace("{httpCode}", statusCode);
+	}
+	
+	public String getCopyButtonString() {
+		return copyButtonString;
+	}
+
+	public String getCopiedButtonString() {
+		return copiedButtonString;
+	}
+
+	public String getExitButtonString() {
+		return exitButtonString;
+	}
+	
+	
+}

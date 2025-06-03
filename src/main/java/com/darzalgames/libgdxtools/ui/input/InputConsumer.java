5182073fdec03592b5b67a2944c075a23ce52dc7
Darzington <32342957+Darzington@users.noreply.github.com>
2025-06-03 10:40:36 -0400

    Persistent UI set up (#79)
    * adjustments to popup and universal label handling
    
    * oops
    
    * use both equals orders to check top of stack
    
    * increment version

--- src/main/java/com/darzalgames/libgdxtools/ui/input/InputConsumer.java
+++ src/main/java/com/darzalgames/libgdxtools/ui/input/InputConsumer.java
@@ -1,8 +1,6 @@
 package com.darzalgames.libgdxtools.ui.input;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
-import com.darzalgames.darzalcommon.data.Tuple;
 import com.darzalgames.libgdxtools.ui.input.inputpriority.InputPriority;
 import com.darzalgames.libgdxtools.ui.input.popup.PopUp;
 
@@ -14,69 +12,69 @@ public interface InputConsumer {
 	 * Choose how to respond to various inputs
 	 * @param input
 	 */
-	public void consumeKeyInput(final Input input);
+	void consumeKeyInput(final Input input);
 
 	/**
 	 * NOTE: If the receiver of this function has a child that is also an InputConsumer on top of it, then instead of setting disabled it should set childrenOnly
 	 * @param isTouchable
 	 */
-	public void setTouchable(Touchable isTouchable);
+	void setTouchable(Touchable isTouchable);
 
 	/**
 	 * Called when this object was just put on the input stack
 	 */
-	public default void gainFocus() {}
+	default void gainFocus() {}
 
 	/**
 	 * Called when this object gains focus back (i.e. something above it on the stack relinquished focus)
 	 */
-	public default void regainFocus() {}
+	default void regainFocus() {}
 
 	/**
 	 * Called when this object has lost focus (sometimes an object does this voluntarily, but it also happens when something else claims priority above this)
 	 */
-	public default void loseFocus() {}
+	default void loseFocus() {}
 
 	/**
 	 * If applicable, put the current relevant object into focus (e.g. selecting the current button so it flashes)
 	 */
-	public void focusCurrent();
+	void focusCurrent();
 
 	/**
 	 * If applicable, clear the currently selected object
 	 */
-	public void clearSelected();
+	void clearSelected();
 
 	/**
 	 * If applicable, select a default object (e.g. the first button in a list, or the only intractable object in the current scene)
 	 */
-	public void selectDefault();
-	
-	public default boolean isPopUp() {
+	void selectDefault();
+
+	default boolean isPopUp() {
 		return false;
 	}
-	
-	public default Tuple<Actor, PopUp> getPopUp() {
-		throw new UnsupportedOperationException(this.toString() + ": A base InputConsumer isn't necessarily a popup! Override isPopUp() & getPopUp() if this truly is one.");
+
+	default PopUp getPopUp() {
+		throw new UnsupportedOperationException(toString() + ": A base InputConsumer isn't necessarily a popup! Override isPopUp() & getPopUp() if this truly is one.");
 	}
 
 	/**
 	 * @return If this object is the topmost UI, is the game paused?
 	 */
-	public default boolean isGamePausedWhileThisIsInFocus() {
+	default boolean isGamePausedWhileThisIsInFocus() {
 		return false;
 	}
 
 	/**
 	 * A convenient shorthand for releasing priority
 	 */
-	public default void releasePriority() {
+	default void releasePriority() {
 		InputPriority.releasePriority(this);
 	}
 
 	/**
 	 * Called every frame, objects should resize their UI in case the window/font have changed size
 	 */
-	public void resizeUI();
-	
-} 
+	void resizeUI();
+
+}
--- src/main/java/com/darzalgames/libgdxtools/ui/input/popup/PopUp.java
+++ src/main/java/com/darzalgames/libgdxtools/ui/input/popup/PopUp.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.darzalgames.darzalcommon.data.Tuple;
 import com.darzalgames.libgdxtools.ui.input.Input;
 import com.darzalgames.libgdxtools.ui.input.InputConsumer;
 
@@ -14,21 +13,21 @@ public interface PopUp extends InputConsumer {
 	/**
 	 * @return Whether or not the popup can be dismissed by pressing "back", or if one of the options must be chosen
 	 */
-	public default boolean canDismiss() {
+	default boolean canDismiss() {
 		return true;
 	}
 
 	/**
 	 * Handles hiding the pop up and unregistering it from the input system
 	 */
-	public default void hideThis() {}
+	default void hideThis() {}
 
 	@Override
-	public default boolean isGamePausedWhileThisIsInFocus() {
+	default boolean isGamePausedWhileThisIsInFocus() {
 		return true;
 	}
 
-	public default void addBackClickListenerIfCanDismiss() {
+	default void addBackClickListenerIfCanDismiss() {
 		if (canDismiss()) {
 			ClickListener rightClickBack = new ClickListener(Buttons.RIGHT) {
 				@Override
@@ -43,17 +42,17 @@ public interface PopUp extends InputConsumer {
 	}
 
 	// These are implemented by default in Actor, which all of our PopUp objects are.
-	public boolean addListener(EventListener listener);
-	public boolean removeListener(EventListener listener);
+	boolean addListener(EventListener listener);
+	boolean removeListener(EventListener listener);
 
 	@Override
 	default boolean isPopUp() {
 		return true;
 	}
-	
+
 	@Override
-	default Tuple<Actor, PopUp> getPopUp() {
-		return new Tuple<>(getAsActor(), this);
+	default PopUp getPopUp() {
+		return this;
 	}
-	public Actor getAsActor();
+	Actor getAsActor();
 }

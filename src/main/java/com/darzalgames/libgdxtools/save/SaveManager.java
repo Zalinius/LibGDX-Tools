72ddcaadfaec33cce417b084ddc262f30a462280
Darzington <nclparmentier@gmail.com>
2025-06-04 10:04:31 -0400

    fun javacod

--- src/main/java/com/darzalgames/libgdxtools/save/SaveManager.java
+++ src/main/java/com/darzalgames/libgdxtools/save/SaveManager.java
@@ -2,5 +2,9 @@ package com.darzalgames.libgdxtools.save;
 
 public interface SaveManager {
 	void save();
+
+	/**
+	 * @return true if loading went fine, false if the save failed to load and/or a new save had to be created
+	 */
 	boolean load();
 }

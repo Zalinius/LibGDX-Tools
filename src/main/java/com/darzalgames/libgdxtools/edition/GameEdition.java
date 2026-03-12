83c936a5ba13d004761c6a5531b029a58e9b104c
Zalinius <31998081+Zalinius@users.noreply.github.com>
2026-03-12 10:54:00 -0400

    add TURBO GameEdition (#123)
    * add TURBO GameEdition
    
    * increase versions
    
    ---------
    
    Co-authored-by: Zalinius <zalinius@darzalgames.com>

--- src/main/java/com/darzalgames/libgdxtools/edition/GameEdition.java
+++ src/main/java/com/darzalgames/libgdxtools/edition/GameEdition.java
@@ -1,11 +1,23 @@
 package com.darzalgames.libgdxtools.edition;
 
 /**
- * Enum for Demo and Full versions of a game
+ * Enum for different editions of games, which differ by content and intent
  */
 public enum GameEdition {
+	/**
+	 * An edition of a game with reduced content and features, available publicly and for free
+	 */
 	DEMO("Demo"),
-	FULL("Full");
+
+	/**
+	 * A complete edition of a game, available publicly and for purchase
+	 */
+	FULL("Full"),
+
+	/**
+	 * A concentrated edition of a game designed to give a thorough experience very quickly, available privately for play-testing
+	 */
+	TURBO("Turbo");
 
 	private final String displayName;
 

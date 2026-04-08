d9bd00debf5acb0ef3dd7ae49bc807e396e807c4
Darzington <32342957+Darzington@users.noreply.github.com>
2026-04-08 11:09:18 -0400

    Bring in the file case name validator from previous games (#128)
    * bring in the file case name validator from previous games
    
    * does this help
    
    * add case for non windows computers
    
    ---------
    
    Co-authored-by: Darzington <team@darzalgames.com>
    Co-authored-by: Zalinius <zalinius@darzalgames.com>

---
+++ src/main/java/com/darzalgames/libgdxtools/assetloading/FileNameCaseValidator.java
@@ -0,0 +1,27 @@
+package com.darzalgames.libgdxtools.assetloading;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+
+public class FileNameCaseValidator implements Consumer<AssetDescriptor<?>> {
+
+	@Override
+	public void accept(AssetDescriptor<?> assetDescriptor) {
+		// Check that the path listed in this file exactly matches the real path for each asset since file handling in Windows is not case sensitive, but both Linux and Steam are.
+		// Linux is using ext4 (which is case sensitive) vs Windows using NTFS (which is not case sensitive) https://stackoverflow.com/questions/34603505/java-file-exists-case-sensitive-jpg-and-jpg
+		try {
+			String specifiedPath = System.getProperty("user.dir") + File.separator + assetDescriptor.fileName;
+			specifiedPath = specifiedPath.replace("/", File.separator);
+			String realPath = Gdx.files.internal(assetDescriptor.fileName).file().getCanonicalPath().replace("desktop", "assets");
+			if (!Gdx.files.internal(assetDescriptor.fileName).file().exists() || !realPath.equals(specifiedPath)) {
+				throw new IllegalArgumentException("\n\nFile not found, possibly a mismatch in upper/lower case?\n" + realPath + "\n     VERSUS\n" + specifiedPath + "\nLook closely!\n\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

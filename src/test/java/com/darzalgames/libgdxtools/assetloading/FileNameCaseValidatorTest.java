495ce99e9f86594f711de083047647debf65fe2c
Darzington <32342957+Darzington@users.noreply.github.com>
2026-04-09 11:39:13 -0400

    add ignored file extensions to the FileNameCaseValidator for generated files (which don't exist) (#129)
    Co-authored-by: Darzington <team@darzalgames.com>

--- src/test/java/com/darzalgames/libgdxtools/assetloading/FileNameCaseValidatorTest.java
+++ src/test/java/com/darzalgames/libgdxtools/assetloading/FileNameCaseValidatorTest.java
@@ -9,11 +9,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Files;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g3d.Model;
 
 class FileNameCaseValidatorTest {
 
 	@Test
-	void testExactCaseMatchBetweenFileNameAndAssetDescriptorDoesNotThrow() {
+	void accept_exactCaseMatchBetweenFileNameAndAssetDescriptor_doesNotThrow() {
 		Gdx.files = new Lwjgl3Files();
 		AssetDescriptor<Texture> textureDescriptor = new AssetDescriptor<>("src/test/resources/com/darzalgames/libgdxtools/gfx/test.png", Texture.class);
 		FileNameCaseValidator fileNameCaseValidator = new FileNameCaseValidator();
@@ -22,11 +23,41 @@ class FileNameCaseValidatorTest {
 	}
 
 	@Test
-	void testMismatchedCaseNamesThrowsIllegalArgumentException() {
+	void accept_mismatchedCaseNames_throwsIllegalArgumentException() {
+		// This test is especially for windows, hitting the "file name comparison" failure branch
 		Gdx.files = new Lwjgl3Files();
 		AssetDescriptor<Texture> textureDescriptor = new AssetDescriptor<>("src/test/resources/com/darzalgames/libgdxtools/gfx/teSt.png", Texture.class);
 		FileNameCaseValidator fileNameCaseValidator = new FileNameCaseValidator();
 
 		assertThrows(IllegalArgumentException.class, () -> fileNameCaseValidator.accept(textureDescriptor));
 	}
+
+	@Test
+	void accept_nonexistantFile_throwsIllegalArgumentException() {
+		// This test is especially for non-windows operating systems, hitting the "non-existant file" failure branch
+		Gdx.files = new Lwjgl3Files();
+		AssetDescriptor<Model> textureDescriptor = new AssetDescriptor<>("src/test/resources/com/darzalgames/libgdxtools/doesNotExist/solarPanel.generated", Model.class);
+		FileNameCaseValidator fileNameCaseValidator = new FileNameCaseValidator();
+
+		assertThrows(IllegalArgumentException.class, () -> fileNameCaseValidator.accept(textureDescriptor));
+	}
+
+	@Test
+	void accept_nonexistantFile_withIgnoredExtension_doesNotThrow() {
+		Gdx.files = new Lwjgl3Files();
+		AssetDescriptor<Model> textureDescriptor = new AssetDescriptor<>("src/test/resources/com/darzalgames/libgdxtools/doesNotExist/solarPanel.generated", Model.class);
+		FileNameCaseValidator fileNameCaseValidator = new FileNameCaseValidator("generated");
+
+		assertDoesNotThrow(() -> fileNameCaseValidator.accept(textureDescriptor));
+	}
+
+	@Test
+	void accept_mismatchedCaseNamesWithIrrelevantIgnoredExtensions_throwsIllegalArgumentException() {
+		Gdx.files = new Lwjgl3Files();
+		AssetDescriptor<Texture> textureDescriptor = new AssetDescriptor<>("src/test/resources/com/darzalgames/libgdxtools/gfx/teSt.png", Texture.class);
+		FileNameCaseValidator fileNameCaseValidator = new FileNameCaseValidator("jpg");
+
+		assertThrows(IllegalArgumentException.class, () -> fileNameCaseValidator.accept(textureDescriptor));
+	}
+
 }

0cc4fecceddf857baece764956bad6ef78cd97fb
Darzington <team@darzalgames.com>
2026-04-20 11:51:41 -0400

    slight tidying

--- src/main/java/com/darzalgames/libgdxtools/trailer/ScreenshotTakerController.java
+++ src/main/java/com/darzalgames/libgdxtools/trailer/ScreenshotTakerController.java
@@ -1,31 +1,37 @@
 package com.darzalgames.libgdxtools.trailer;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public class ScreenshotTakerController extends Actor {
+public class ScreenshotTakerController {
 
 	private final String screenshotOutputPath;
 
 	private String thisFrameScreenshotName;
 
+	/**
+	 * A tidy system for flagging the current frame as one to screenshot, and later taking said screenshot after rendering
+	 * @param screenshotOutputPath where to output screenshots to
+	 */
 	public ScreenshotTakerController(String screenshotOutputPath) {
-		super();
 		this.screenshotOutputPath = screenshotOutputPath;
 	}
 
-	@Override
-	public void draw(Batch batch, float parentAlpha) {
-		super.draw(batch, parentAlpha);
+	/**
+	 * Takes a screenshot if {@link #setThisFrameScreenshotName(String screenshotName)} was called this frame.
+	 * This must be called AFTER everything in the screenshot has been drawn, aka at the end of render(), and is safe to call every frame.
+	 */
+	public void takeScreenshotIfQueuedUp() {
 		if (thisFrameScreenshotName != null) {
-			Gdx.app.log("Screenshot Taker Main", "Taking sceenshot: " + thisFrameScreenshotName);
+			Gdx.app.log(ScreenshotTakerController.class.getSimpleName().toUpperCase(), "Taking sceenshot: " + thisFrameScreenshotName);
 			ScreenshotTaker.takeScreenshot(screenshotOutputPath + thisFrameScreenshotName);
 			thisFrameScreenshotName = null;
 		}
 	}
 
-	public void takeScreenshot(String screenshotName) {
+	/**
+	 * @param screenshotName Set the file name for the screenshot to be taken this frame
+	 */
+	public void setThisFrameScreenshotName(String screenshotName) {
 		thisFrameScreenshotName = screenshotName + ".png";
 	}
 

c87003e070ea2b80c3684bfb76cc4cbda8085a71
Darzington <team@darzalgames.com>
2026-06-29 09:52:37 -0400

    popup touchable toggling

--- src/main/java/com/darzalgames/libgdxtools/ui/input/navigablemenu/PopUpMenu.java
+++ src/main/java/com/darzalgames/libgdxtools/ui/input/navigablemenu/PopUpMenu.java
@@ -45,6 +45,7 @@ public interface PopUpMenu extends PopUp, VisibleInputConsumer {
 
 	default void slideIn(Runnable superGainFocus, Runnable menuFocusCurrent) {
 		superGainFocus.run();
+		setTouchable(Touchable.disabled);
 		if (slidesInAndOut()) {
 			float startX = getAsActor().getX();
 			float startY = getAsActor().getY();
@@ -62,6 +63,7 @@ public interface PopUpMenu extends PopUp, VisibleInputConsumer {
 	@Override
 	default void hideThis() {
 		releasePriority();
+		setTouchable(Touchable.disabled);
 		if (slidesInAndOut()) {
 			getAsActor().addAction(
 					Actions.sequence(
